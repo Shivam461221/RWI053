f685517e5db13c178151b2273b25bc80044bc14f
Shivam Lowanshi <shivamlowanshi@gmail.com>
2024-05-10 10:32:17 +0530

    chat-app

---
+++ ChatApplication/src/main/java/com/shivam/models/Message.java
@@ -0,0 +1,28 @@
+package com.shivam.models;
+
+public class Message {
+	private String name;
+	private String content;
+	public Message() {
+		super();
+	}
+	public Message(String name, String content) {
+		super();
+		this.name = name;
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
